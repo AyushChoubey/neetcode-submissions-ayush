e64ad8c7a54f4aff50a74ad6797989ea2a26a5ee
AC_4 <choubey.ayush875@gmail.com>
2026-05-18 11:46:55 +0530

    Add: word-ladder - submission-4

---
+++ Data Structures & Algorithms/word-ladder/submission-4.py
@@ -0,0 +1,58 @@
+from collections import deque
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+
+
+        if endWord not in wordList:
+            return 0
+        
+        q = deque([beginWord])
+        
+        graph = {}
+        def differs_by_one(w1, w2):
+            if len(w1) != len(w2):
+                return False
+            
+            diff = 0
+            for c1, c2 in zip(w1, w2):
+                if c1 != c2:
+                    diff += 1
+                if diff > 1:        # early exit!
+                    return False
+            
+            return diff == 1
+
+        
+
+        
+        cnt=1
+        visited = set()
+        wordset = set(wordList)
+        while q:
+
+            for _ in range(len(q)):
+                w1 = q.popleft()  
+                
+                visited.add(w1)
+                if w1 == endWord:
+                    return cnt
+
+                for i in range(len(w1)):
+                    for c in 'abcdefghijklmnopqrstuvwxyz':
+                        w2 = w1[:i]+c+w1[i+1:]
+                        if w2 in wordset and w2 not in visited:
+                            q.append(w2)
+                
+                # print(q)
+                if not q:
+                    return 0
+
+            cnt+=1
+        
+        
+
+
+
+
+        
+        
