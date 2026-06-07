8f04801841d72f1c0744c85947c6fa95110b76ad
AC_4 <choubey.ayush875@gmail.com>
2026-06-07 13:08:00 +0530

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # 1d solution 
+
+        T = [amount+1] *(amount+1)
+        T[0] = 0
+
+        for i in range(1, amount+1):
+            for c in coins:
+                if c<=i:
+                    T[i] = min(T[i],1+T[i-c]) # this inner loop is same as T[i] = min(1 + T[i - coin] for coin in coins)
+                    # because we are already storing minimum till now to the T[i] so if we pick the  min(T[i],1+T[i-c])  it gives us the minimum coin 
+
+        print(T)
+        
+        return T[-1] if T[-1]!= amount+1 else -1
