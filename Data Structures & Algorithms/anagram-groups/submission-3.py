e2e1c20649471ed9a8d3944d113f5275fc18543f
AC_4 <choubey.ayush875@gmail.com>
2026-08-07 00:14:37 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        group = {}
+        
+        for i in strs:
+            alpha_dict = {
+            'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0,
+            'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
+            'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0,
+            'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0,
+            'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0,
+            'z': 0}
+
+            for j in i:
+
+                alpha_dict[j]+=1
+
+            k = str(alpha_dict)
+
+            if k in group:
+                group[k].append(i)
+
+            else:
+                group[k] = [i]
+
+        result = []
+        for i in group :
+            result.append(group[i])
+        return result
